3c19234db8944db6290937cd54abbcecd6291515
lizhanmit <lizhan_mit@outlook.com>
2022-12-27 19:31:51 +0800

    Create image spark-stream-processing-model.png.

---
+++ src/main/scala/org/example/wordCount/StreamingWordCount.scala
@@ -0,0 +1,37 @@
+package org.example.wordCount
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{col, explode, split}
+
+object StreamingWordCount extends Serializable {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .master("local[3]")
+      .config("spark.sql.shuffle.partitions", 3)
+      .config("spark.streaming.stopGracefullyOnShutdown", true)
+      .appName("Streaming Word Count")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val lineDF: DataFrame = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", "9999")
+      .load()
+
+    val wordCountDF: DataFrame = lineDF.withColumn("values", split(col("value"), " "))
+      .select(explode(col("values")).as("word"))
+      .groupBy("word").count()
+
+
+    val wordCountQuery = wordCountDF.writeStream
+      .format("console")
+      .option("checkpointLocation", "checkpointDir")
+      .outputMode("complete")
+      .start()
+
+    wordCountQuery.awaitTermination()
+  }
+}
